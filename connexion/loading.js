a06f82c5058a3980f1b391135474d44f27abf2ac
MedFrio <med.friouichen@gmail.com>
2024-10-01 14:26:43 +0200

    bdd

--- connexion/loading.js
+++ connexion/loading.js
@@ -2,7 +2,8 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
     host: 'localhost',
     user: 'esgi_game', 
-    database: 'test_bdd_projet' 
+    database: 'test_bdd_projet', 
+    password:'esgigame'
 });
 conn.connect(function (err) {
     if (err) throw err;
