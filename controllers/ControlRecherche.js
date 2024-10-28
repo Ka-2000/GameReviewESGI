390b0bbd9a93977e8ed44888ff5a03849963fcc3
Cengokill <cengokillian@hotmail.com>
2024-10-28 11:17:27 +0100

    ajout code requête pour recherche

--- controllers/ControlRecherche.js
+++ controllers/ControlRecherche.js
@@ -1,73 +1,35 @@
 var modelRecherche = require('../models/ModelRecherche');
+const { get } = require('../routes/route');
 module.exports = {
-    
-    // affichage accueil avec les différents charts (à prevoir, stock effectif et part pathologies)
     afficher_recherche: function (req, res) {
         modelRecherche.afficher_recherche(function (data, data2, data3) {
-            // chart stock
-            var lesMeds = []
-            var lesDonnesMeds = []
-            for (let i = 0; i < data3.length; i++) {
-                lesMeds.push(data3[i].Medicaments_libelle)
-                lesDonnesMeds.push(data3[i].Medicaments_qte)
-            }
-            
-            // chart pathologies
-            var lesPath = []
-            var lesDonnesPaths = []
-            for (let i = 0; i < data2.length; i++) {
-                lesPath.push(data2[i].Pathologies_libelle)
-                lesDonnesPaths.push(data2[i].total)
-            }
-            
-            function daysInMonth(month, year) {
-                return new Date(year, month, 0).getDate();
-            }
-            
-            // chart stock a prevoir
-            lesMois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-            var d = new Date();
-            var annee = d.getFullYear
-            var prochainMois = [lesMois[d.getMonth()]]
-            var prochainMoisEnNombre = [d.getMonth() + 1]
-            var joursProchainsMoisEnnombre = [daysInMonth(d.getMonth() + 1, new Date().getFullYear())]
-            for (let i = 0; i < 6; i++) {
-                d.setMonth(d.getMonth() + 1);
-                prochainMois.push(lesMois[d.getMonth()])
-                prochainMoisEnNombre.push(d.getMonth() + 1)
-                joursProchainsMoisEnnombre.push(daysInMonth(d.getMonth(), new Date().getFullYear()))
-            }
-            
-            let test = []
-            for (let i = 0; i < lesMeds.length; i++) {
-                testArray = Array.from({ length: 7 }, () => Math.floor(Math.random() * 20));
-                test.push([testArray])
-                test[i].push(lesMeds[i])
-            }
-            
-            modelStock.afficher_liste_stocks(function (data, data2, data3) {
-                lesStock = []
-                for (i in data2) {
-                    lesStock.push([data2[i].idMedicament, data2[i].Medicaments_libelle, data2[i].Medicaments_qte, data2[i].stock_necessaire])
+
+            // Récupère toutes les infos des jeux à partir d'un titre recherché par l'utilisateur
+            const getGameDataQuery = `
+                SELECT *
+                FROM jeux
+                WHERE titre LIKE ?
+            `;
+            // Récupère getGameDataQuery en ajoutant un filtre sur la date : jeux sortis après 2024-01-01
+            const getGameDateQueryNouveaute = getGameDataQuery + ` AND date_de_sortie > '2024-01-01'`;
+
+            // Récupère getGameDataQuery en ajoutant un filtre sur la note du jeu en fonction de la variable noteValue
+            const getGameDateQueryNote = getGameDataQuery + ` AND note_moyenne > ?`;
+
+            // Ajoute des % avant et après le terme de recherche pour rechercher des jeux contenant le terme
+            const searchTerm = `%${userInput}%`;
+
+            //TODO : si la case Nouveauté est cochée, on utilise getGameDateQueryNouveaute
+            //TODO : on utilise getGameDateQueryNote en fonction de la note minimale moyenne que l'utilisateur désire
+
+            bd.query(getGameDataQuery, [searchTerm], function (err, getGameResult) {
+                if (err) {
+                    console.error("Erreur lors de l'exécution de la requête : ", err);
+                    return;
                 }
-                
-                for (i in data3) {
-                    lesStock.push([data3[i].Medicaments_id, data3[i].Medicaments_libelle, data3[i].Medicaments_qte, 0])
-                }
-                
-                for (i in data) {
-                    lesStock[i][3] = lesStock[i][3] + data[i].stock_necessaire
-                }
-                lesMeds = []
-                lesStockEffectif =  []
-                for (i in lesStock){
-                    lesMeds.push(lesStock[i][1])
-                    lesStockEffectif.push(lesStock[i][2]-lesStock[i][3])
-                }
-                
-                
-                res.render('./recherche', { lesMeds, lesStockEffectif: JSON.stringify(lesStockEffectif), lesStock, test, lesPath: lesPath, lesDonnesPaths: JSON.stringify(lesDonnesPaths), prochainMois: prochainMois, prochainMoisEnNombre: JSON.stringify(prochainMoisEnNombre), lesMeds: lesMeds, lesDonnesMeds: JSON.stringify(lesDonnesMeds), contenu: data3, titre: "Liste des clients" })
+                console.log("Résultats : ", getGameResult);
             });
-        });
-    },
-}
+        }
+    }
+};
+   
